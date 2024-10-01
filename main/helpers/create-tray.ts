a5b2498aa44c8ae5b567247623fcbc2250bcc046
hyeongyu An <hyeongyu2656@gmail.com>
2024-10-01 19:19:23 +0900

    update: 빌드 환경에서도 Tray 아이콘 노출되도록 수정

--- main/helpers/create-tray.ts
+++ main/helpers/create-tray.ts
@@ -1,9 +1,23 @@
-import { nativeImage, Tray } from "electron";
+import path from 'path';
+import { nativeImage, Tray } from 'electron';
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const getResourcesPath = (pathname: string) => {
+  return path.join(isProd ? process.resourcesPath : process.cwd(), pathname);
+};
+
+const getTrayIcon = () => {
+  const icon = nativeImage.createFromPath(getResourcesPath('assets/tray-icon.png'));
+  const trayIcon = icon.resize({ width: 18 });
+
+  trayIcon.setTemplateImage(true);
+
+  return trayIcon;
+};
 
 export const createTray = () => {
-  const icon = nativeImage.createFromPath('resources/tray-icon.png');
-  const trayIcon = icon.resize({ width: 18 });
-  trayIcon.setTemplateImage(true);
+  const trayIcon = getTrayIcon();
 
   const tray = new Tray(trayIcon);
 
