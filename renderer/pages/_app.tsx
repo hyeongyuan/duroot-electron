01c3b7b707537fa1126cb9dacd9a2314e3cbbffe
hyeongyu An <hyeongyu2656@gmail.com>
2024-10-02 23:09:45 +0900

    update: 기본 레이아웃 추가

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import type { AppProps } from 'next/app'
+import React from 'react';
+import type { AppProps } from 'next/app';
 
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="bg-[#22272e] text-[#adbac7] h-screen rounded-md overflow-hidden">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
