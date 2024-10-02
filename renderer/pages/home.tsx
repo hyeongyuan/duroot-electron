52386e719d5e63f9bf12b776dbe0150bf6c84154
hyeongyu An <hyeongyu2656@gmail.com>
2024-10-02 23:35:57 +0900

    feat: 인증 토큰으로 사용자 정보를 조회하는 동선 추가

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,70 +1,24 @@
-import { useState } from 'react';
-import Image from 'next/image';
-import { AxiosError } from 'axios';
-
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 import { fetchUser } from '../apis/github';
-import { Input } from '../components/input';
-
-const ERROR_MESSAGE: Record<number, string> = {
-  401: 'It is not a valid token.',
-};
 
 export default function HomePage() {
-  const [inputValue, setInputValue] = useState('');
-  const [message, setMessage] = useState('');
+  const router = useRouter();
+
+  useEffect(() => {
+    window.ipc.invoke('storage:get', 'auth.token')
+      .then(async (token) => {
+        try {
+          const user = await fetchUser(token);
+          
+          console.log(user);
   
-  const handleSubmit = async () => {
-    setMessage('');
-    try {
-      await fetchUser(inputValue);
+          router.replace('/pulls');
+        } catch (error) {
+          router.replace('/auth');
+        }
+      });
+  }, []);
 
-      await window.ipc.invoke('storage:set', 'auth.token', inputValue);
-
-      console.log('success to save token');
-    } catch (error) {
-      if (error instanceof AxiosError) {
-        const { status } = error.response || { status: 599 };
-        setMessage(ERROR_MESSAGE[status] || error.message);
-      }
-    }
-  };
-
-  return (
-    <div className='flex flex-1 justify-center'>
-      <div className="my-28">
-        <div className="mb-24">
-          <Image
-            src="/images/logo.png"
-            alt="Duroot Logo"
-            width={240}
-            height={80}
-            unoptimized
-            style={{ margin: '0 auto' }}
-          />
-        </div>
-        <div className='flex'>
-          <div className="mr-2">
-            <Input
-              placeholder="Please enter your token"
-              value={inputValue}
-              autoComplete="off"
-              autoCapitalize="off"
-              autoCorrect="off"
-              onChange={(event) => setInputValue(event.target.value)}
-            />
-            {!!message && (
-              <p className="ml-1 mt-2 text-xs text-[#e5534b]">{message}</p>
-            )}
-          </div>
-          <button
-            className="bg-[#347d39] hover:bg-[#46954a] text-[#ffffff] text-sm rounded h-[32px] px-4 border border-[rgba(205,217,229,0.1)]"
-            onClick={handleSubmit}
-            disabled={!inputValue}
-          >
-            Submit
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+  return null;
 }
---
+++ renderer/pages/pulls.tsx
@@ -0,0 +1,7 @@
+export default function PullsPage() {
+  return (
+    <div>
+      <h1>Pull Requests</h1>
+    </div>
+  );
+}
