e4302b63372ec244a8d4266215dcc4d38aff8124
hyeongyu An <hyeongyu2656@gmail.com>
2024-11-10 00:58:19 +0900

    update: 토큰을 생성 관련 안내 문구 추가

--- renderer/pages/auth.tsx
+++ renderer/pages/auth.tsx
@@ -5,6 +5,7 @@ import { AxiosError } from 'axios';
 
 import { fetchUser } from '../apis/github';
 import { Input } from '../components/input';
+import { Anchor } from '../components/common/anchor';
 
 const ERROR_MESSAGE: Record<number, string> = {
   401: 'It is not a valid token.',
@@ -32,41 +33,51 @@ export default function AuthPage() {
   };
 
   return (
-    <div className='flex flex-1 justify-center'>
-      <div className="my-28">
-        <div className="mb-24">
-          <Image
-            src="/images/logo.png"
-            alt="Duroot Logo"
-            width={240}
-            height={80}
-            unoptimized
-            style={{ margin: '0 auto' }}
-          />
-        </div>
-        <div className='flex'>
-          <div className="mr-2">
-            <Input
-              placeholder="Please enter your token"
-              value={inputValue}
-              autoComplete="off"
-              autoCapitalize="off"
-              autoCorrect="off"
-              onChange={(event) => setInputValue(event.target.value)}
+    <>
+      <div className='flex flex-1 justify-center'>
+        <div className="my-28">
+          <div className="mb-24">
+            <Image
+              src="/images/logo.png"
+              alt="Duroot Logo"
+              width={240}
+              height={80}
+              unoptimized
+              style={{ margin: '0 auto' }}
             />
-            {!!message && (
-              <p className="ml-1 mt-2 text-xs text-[#e5534b]">{message}</p>
-            )}
           </div>
-          <button
-            className="bg-[#347d39] hover:bg-[#46954a] text-[#ffffff] text-sm rounded h-[32px] px-4 border border-[rgba(205,217,229,0.1)]"
-            onClick={handleSubmit}
-            disabled={!inputValue}
-          >
-            Submit
-          </button>
+          <div className='flex'>
+            <div className="mr-2">
+              <Input
+                placeholder="Please enter your token"
+                value={inputValue}
+                autoComplete="off"
+                autoCapitalize="off"
+                autoCorrect="off"
+                onChange={(event) => setInputValue(event.target.value)}
+              />
+              {!!message && (
+                <p className="ml-1 mt-2 text-xs text-[#e5534b]">{message}</p>
+              )}
+            </div>
+            <button
+              className="bg-[#347d39] hover:bg-[#46954a] text-[#ffffff] text-sm rounded h-[32px] px-4 border border-[rgba(205,217,229,0.1)]"
+              onClick={handleSubmit}
+              disabled={!inputValue}
+            >
+              Submit
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      <div>
+        <p className="text-xs text-center">
+          Don't have a token?{' '}
+          <Anchor className="text-[#4493f8] underline cursor-pointer" href="https://github.com/settings/tokens">
+            Generate here
+          </Anchor>
+        </p>
+      </div>
+    </>
   );
 }
