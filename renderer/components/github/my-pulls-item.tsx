f4c99baaf0e03a36dd8fc56e3207a26bac38fa32
hyeongyu An <hyeongyu2656@gmail.com>
2025-08-03 01:36:15 +0900

    update: MyPullsItem 컴포넌트가 뷰포트 내 들어왔을 때, 데이터 조회하도록 수정

--- renderer/components/github/my-pulls-item.tsx
+++ renderer/components/github/my-pulls-item.tsx
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
-import { Label } from '../common/label';
-import { useAuthStore } from '../../stores/auth';
-import { ApprovedMark } from '../common/approved-mark';
 import { fetchReviewCount } from '../../apis/github';
+import { useIntersectionObserver } from '../../hooks/use-intersection-observer';
+import { useAuthStore } from '../../stores/auth';
 import { Anchor } from '../common/anchor';
+import { ApprovedMark } from '../common/approved-mark';
+import { Label } from '../common/label';
 
 interface MyPullsItemProps {
   title: string;
@@ -22,16 +23,22 @@ interface MyPullsItemProps {
 export function MyPullsItem({ title, titleUrl, subtitle, subtitleUrl, labels, pullRequestUrl, caption, draft }: MyPullsItemProps) {
   const { data } = useAuthStore();
 
+  const { targetRef, hasIntersected } = useIntersectionObserver<HTMLLIElement>({
+    threshold: 0.1,
+    rootMargin: '50px',
+    triggerOnce: true,
+  });
+
   const { data: reviewCount } = useQuery({
     queryKey: ['my-pulls', pullRequestUrl, data.user.login],
     queryFn: () => fetchReviewCount(data.token, pullRequestUrl, data.user.login),
-    enabled: !!data
+    enabled: !!data && hasIntersected,
   });
 
   const allApproved = reviewCount && reviewCount.approved === reviewCount.total;
 
   return (
-    <li className="flex flex-col px-4 py-2">
+    <li ref={targetRef} className="flex flex-col px-4 py-2">
       <div className="flex items-center">
         <Anchor
           className="text-[#768390] text-xs leading-5 line-clamp-1 break-all hover:underline hover:underline-offset-1 pr-1"
