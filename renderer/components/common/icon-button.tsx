9998d2360b500ca8bd6893075a7bd42353373360
hyeongyu An <hyeongyu2656@gmail.com>
2025-08-04 20:18:19 +0900

    fix: 툴팁 스타일 적용 방식 변경

--- renderer/components/common/icon-button.tsx
+++ renderer/components/common/icon-button.tsx
@@ -8,7 +8,7 @@ interface IconButtonProps {
 export function IconButton({ onClick, children, tooltip, disabled }: IconButtonProps) {
   return (
     <div
-      className={tooltip ? 'tooltip tooltip-bottom before:bg-[#373e47] before:text-xs after:bg-[#373e47]' : ''}
+      className="tooltip tooltip-bottom"
       data-tip={tooltip}
     >
       <button
