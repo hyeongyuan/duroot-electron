63b73e9613ce4465336f702d687091ee20223c6d
hyeongyu An <hyeongyu2656@gmail.com>
2025-08-05 09:41:27 +0900

    update: 인증 에러 시, 재시도 하지 않도록 수정

--- renderer/providers/query-provider.tsx
+++ renderer/providers/query-provider.tsx
@@ -1,8 +1,20 @@
 import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { isAuthorizedError } from '../apis/github';
 
 export const QueryProvider = ({ children }: { children: React.ReactNode}) => {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry(failureCount, error) {
+          if (isAuthorizedError(error)) {
+            return false;
+          }
+          return failureCount < 3;
+        },
+      }
+    },
+  }));
   return (
     <QueryClientProvider client={queryClient}>
       {children}
