3f4ce11040f245cd367f38c9403e7029802dcb88
hyeongyu An <hyeongyu2656@gmail.com>
2025-12-14 02:22:36 +0900

    update: 불필요한 안내 문구 제거

--- renderer/pages/auth.tsx
+++ renderer/pages/auth.tsx
@@ -2,11 +2,9 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-import { Anchor } from "../components/common/anchor";
 import { SignInButton } from "../components/github/sign-in-button";
 import type { GithubAccessToken } from "../types/github";
 import { ipcHandler } from "../utils/ipc";
-
 
 export default function AuthPage() {
   const router = useRouter();
@@ -36,32 +34,22 @@ export default function AuthPage() {
   };
 
   return (
-    <>
-      <div className="flex flex-1 justify-center">
-        <div className="my-28">
-          <div className="mb-24">
-            <Image
-              src="/images/logo.png"
-              alt="Duroot Logo"
-              width={240}
-              height={80}
-              unoptimized
-              style={{ margin: "0 auto" }}
-            />
-          </div>
-          <div className="flex">
-            <SignInButton isLoading={isAuthorizing} onClick={handleGithubSignIn} />
-          </div>
+    <div className="flex flex-1 justify-center">
+      <div className="my-28">
+        <div className="mb-24">
+          <Image
+            src="/images/logo.png"
+            alt="Duroot Logo"
+            width={240}
+            height={80}
+            unoptimized
+            style={{ margin: "0 auto" }}
+          />
+        </div>
+        <div className="flex">
+          <SignInButton isLoading={isAuthorizing} onClick={handleGithubSignIn} />
         </div>
       </div>
-      <div>
-        <p className="text-center text-xs">
-          Don"t have a token?{" "}
-          <Anchor className="cursor-pointer text-[#4493f8] underline" href="https://github.com/settings/tokens">
-            Generate here
-          </Anchor>
-        </p>
-      </div>
-    </>
+    </div>
   );
 }
