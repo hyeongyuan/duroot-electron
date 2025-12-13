61c6190c271f03f9c29ceb593000b58843a14371
hyeongyu An <hyeongyu2656@gmail.com>
2025-12-14 02:00:15 +0900

    chore: 인증 정보 관련 타입 추가

--- renderer/types/github.ts
+++ renderer/types/github.ts
@@ -43,3 +43,12 @@ export interface GithubPull {
 	requested_reviewers: GithubUser[];
 	user: GithubUser;
 }
+
+export type GithubAccessToken = {
+  access_token: string;
+  expires_in: number;
+  refresh_token: string;
+  refresh_token_expires_in: number;
+  token_type: string;
+  scope: string;
+};
